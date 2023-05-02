dfc682de23b51610f12d0f15ef0fa493830e3d88
erand <erand88919@gmail.com>
2023-05-02 17:21:38 -0400

    start docker

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">Wonderful speed!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
